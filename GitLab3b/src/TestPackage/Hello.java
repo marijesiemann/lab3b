a1c9e2d9d383046b0bb3de0d498d8cd6462bee53
Marije.Siemann <Marije.Siemann@Marije-L>
2016-12-09 10:41:23 +0100

    master branch changes

--- GitLab3b/src/TestPackage/Hello.java
+++ GitLab3b/src/TestPackage/Hello.java
@@ -5,10 +5,15 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Hello change");
+		System.out.println("Hello master");
 	}
 	
 	public int squared(int i) {
 		return i^5;
+	}
+	
+	public int cubic(int i) {
+		return i^3;
 	}
 
 }
