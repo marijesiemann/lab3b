67bf6c56b9db210c9068b7d42e085a09bcfc0880
Marije.Siemann <Marije.Siemann@Marije-L>
2016-12-09 10:27:16 +0100

    push test different place

--- GitLab3b/src/TestPackage/Hello.java
+++ GitLab3b/src/TestPackage/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Hello change");
+		System.out.println("Hello push test");
 	}
 	
 	public int squared(int i) {
